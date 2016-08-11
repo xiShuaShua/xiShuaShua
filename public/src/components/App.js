f257fcd17e5ef71eaadfc74b424ef7fe10e6235e
banshengbushu <wangdanna1995@outlook.com>
2016-08-12 01:22:31 +0800

    webpack succeed

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import Login from "./Login";
-import Second from "./Second-page";
 class App extends Component {
   render() {
     return (
-        <Second/>
+        <Login/>
     )
   }
 }
