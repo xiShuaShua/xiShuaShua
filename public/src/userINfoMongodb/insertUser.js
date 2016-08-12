898bc2364ca44c2fc1181aa02ce38b00e8abbbf0
banshengbushu <wangdanna1995@outlook.com>
2016-08-12 20:19:16 +0800

    create userInfo mongoDB

---
+++ public/src/userINfoMongodb/insertUser.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/xiShuaShua';
+
+var insertData = function (db,data, callback) {
+    var collection = db.collection('user');
+
+    collection.insert(data, function (err, result) {
+        if (err) {
+            console.log('Error:' + err);
+            return;
+        }
+        callback(result);
+    });
+};
+
+exports.insertUser = function (req,res) {
+    MongoClient.connect(DB_CONN_STR, function (err, db) {
+        let data = req.body;
+        console.log(req.body);
+
+        insertData(db,data,function (result) {
+            db.close();
+        });
+    });
+};
