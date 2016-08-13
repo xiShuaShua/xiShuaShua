f73bd46932ee09c95c535a8e55ba8362befe5507
banshengbushu <wangdanna1995@outlook.com>
2016-08-13 11:36:49 +0800

    refactor

--- public/src/main.js
+++ public/src/main.js
@@ -4,18 +4,20 @@ import Second from "./components/Second-page";
 import Best from "./components/Best";
 import Register from "./components/Register";
 import App from "./components/App";
+import Forget from "./components/Forget";
 
 ReactDOM.render(
     <ReactRouter.Router>
-        <ReactRouter.Route path="/" component = {App}>
-            <ReactRouter.IndexRoute component = {Login}/>
-            <ReactRouter.Route path="/first" component = {First}/>
-            <ReactRouter.Route path="/second" component = {Second}/>
-            <ReactRouter.Route path="/login" component = {Login}/>
-            <ReactRouter.Route path="/best" component = {Best}/>
-            <ReactRouter.Route path="/register" component = {Register}/>
+        <ReactRouter.Route path="/" component={App}>
+            <ReactRouter.IndexRoute component={Login}/>
+            <ReactRouter.Route path="/first" component={First}/>
+            <ReactRouter.Route path="/second" component={Second}/>
+            <ReactRouter.Route path="/login" component={Login}/>
+            <ReactRouter.Route path="/best" component={Best}/>
+            <ReactRouter.Route path="/register" component={Register}/>
+            <ReactRouter.Route path="/forget" component={Forget}/>
         </ReactRouter.Route>
 
     </ReactRouter.Router>
-,document.getElementById("app"));
+    , document.getElementById("app"));
 
