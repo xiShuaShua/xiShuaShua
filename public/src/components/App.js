e39bb6a50f74ee1dcf4d0db05fb50499cf97c405
banshengbushu <wangdanna1995@outlook.com>
2016-08-15 11:37:33 +0800

    code review

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react"
 class App extends Component {
-    constructor(props){``
+    constructor(props){
         super(props);
         this.state = {
             id:0,
