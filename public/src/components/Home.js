2a9b6b54e29d0891893ebd87615a6a936053b814
banshengbushu <wangdanna1995@outlook.com>
2016-08-16 09:54:12 +0800

    add home and css

--- public/src/components/Home.js
+++ public/src/components/Home.js
@@ -1,15 +1,65 @@
 const Home = React.createClass({
+    getInitialState: function () {
+        return {
+            flag: 0
+        }
+    },
+
+    toggle: function () {
+        if (this.state.flag === 0) {
+            $('#personal').css({color: '#3547A2'});
+            $('#sideBar').animate({left: "0"}, 'slow');
+            this.setState({flag: 1});
+        } else {
+            $('#personal').css({color: '#ffffff'});
+            $('#sideBar').animate({left: "-170"}, 'slow');
+            this.setState({flag: 0});
+        }
+    },
+
     render: function () {
-        return <div className="row my-nav my-bg my-white-color">
-            <ul className="nav">
-                <ReactRouter.Link to = "/menu">
-                    <li className="col-xs-4"><span className="glyphicon glyphicon-user">个人中心</span></li>
-                </ReactRouter.Link>
-                <li className="col-xs-4 text-center">洗刷刷</li>
-                <li className="col-xs-4 text-right"><span className="glyphicon glyphicon-heart">收藏</span></li>
-            </ul>
+        const clientHeight = $(window).height() - 50;
+        const sideBar = {
+            height: clientHeight,
+            width: '200',
+            backgroundColor: '#000',
+            zIndex: 1
+        };
+
+        return <div className="row my-nav my-bg my-white-color my-home">
+            <div className="row my-nav-padding">
+                <ul className="nav">
+                    <li className="col-xs-4" id="personal" onClick={this.toggle}><span
+                        className="glyphicon glyphicon-user">个人中心</span></li>
+                    <li className="col-xs-4 text-center">洗刷刷</li>
+                    <li className="col-xs-4 text-right"><span className="glyphicon glyphicon-heart">收藏</span></li>
+                </ul>
+            </div>
             <Top />
             <Body />
+            <div style={sideBar} className="row my-side-bar" id="sideBar">
+                <ul className="nav text-left">
+                    <ReactRouter.Link to="/login">
+                        <li className="my-white-color"><span
+                            className="glyphicon glyphicon-plus my-logo-padding"></span>用户登陆
+                        </li>
+                    </ReactRouter.Link>
+                    <ReactRouter.Link to="/register">
+                        <li className="my-white-color"><span
+                            className="glyphicon glyphicon-pencil my-logo-padding my-white-color"></span>用户注册
+                        </li>
+                    </ReactRouter.Link>
+                    <li><span className="glyphicon glyphicon-heart-empty my-logo-padding"></span>个性装扮</li>
+                    <li><span className="glyphicon glyphicon-bookmark my-logo-padding"></span>激活会员</li>
+                    <li><span className="glyphicon glyphicon-usd my-logo-padding"></span>会员充值</li>
+                </ul>
+                <div className="row my-sidebar-bottom">
+                    <div className="col-xs-6"><span className="my-setting-padding">设置</span></div>
+                    <ReactRouter.Link to="/login">
+                        <div className="col-xs-6"><span className="my-setting-padding">退出</span></div>
+                    </ReactRouter.Link>
+                </div>
+            </div>
         </div>
     }
 });
@@ -18,11 +68,11 @@ const Top = React.createClass({
     render: function () {
         return <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
             <ol className="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"> </li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"> </li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"> </li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"> </li>
+                <li data-target="#carousel-example-generic" data-slide-to="4"> </li>
 
             </ol>
 
@@ -86,8 +136,7 @@ const Body = React.createClass({
                 </div>
                 <div className="col-xs-6 my-table">
                     <div className="my-server my-purple center-block my-right">
-                        <span
-                            className="glyphicon glyphicon-phone-alt glyphicon glyphicon-tower my-white-color"> </span>
+                        <span className="glyphicon glyphicon-phone-alt glyphicon glyphicon-tower my-white-color"> </span>
                     </div>
                     <div className="my-text-right">众包服务</div>
                 </div>
@@ -110,4 +159,4 @@ const Body = React.createClass({
     }
 });
 
-export default Home
+export default Home;
